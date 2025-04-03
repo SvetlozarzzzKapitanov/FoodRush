8defdf577d146a592900bb1eda3c7550ba341b8b
SvetlozarzzzKapitanov <svetlio04@abv.bg>
2025-04-03 20:42:01 +0300

    Relocated the Search bar data in its own component

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,26 +1,11 @@
 import React from 'react';
 import './HeroSection.css';
+import SearchBar from './SearchBar';
 
 const HeroSection: React.FC = () => {
-    const handleSearch = () => {
-        const query = (document.getElementById("search-input") as HTMLInputElement).value;
-        if (query) {
-            alert(`Searching for ${query}`);
-        }
-    }
   return (
       <div className="hero-section">
-        <div className="search-bar">
-          <input
-              id ="search-input"
-              type="text"
-              placeholder="Hungry? Let’s fix that."
-          />
-
-            <button className="search-icon" onClick={handleSearch}>
-                <img src="/public/SearchLensIcon.svg" alt="Search" />
-            </button>
-        </div>
+        <SearchBar />
       </div>
   );
 };
